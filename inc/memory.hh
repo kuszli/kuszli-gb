3bce2d6158cafde49c569b5f22da7940164471bb
Dawid Olszewski <kuszli@o2.pl>
2019-09-10 19:46:05 +0200

    Added game saving

--- inc/memory.hh
+++ inc/memory.hh
@@ -5,30 +5,70 @@
 #include<fstream>
 #include<iostream>
 #include<string>
+#include<cmath>
+
+enum _mbc_type{
+	none,
+	mbc1,
+	mbc2,
+	mbc3,
+	mbc5,
+};
 
 class _memory{
 
-uint8_t* memory;
-uint8_t* rom_banks;
-std::fstream* rom;
-uint8_t curr_bank;
-uint8_t dummy;
-bool mbc;
+	uint8_t* memory;
+	uint8_t* rom_banks;
+	uint8_t* external_ram;
+	std::fstream* rom;
+
+	std::string _rom_name;
+	uint8_t latch;
+	uint8_t dummy;
+	uint8_t ram_type;	
+	bool mbc;
+	bool ex_ram;
+	bool ram_enable;
+	bool dma_time;
+	uint8_t mode_select;
+	uint16_t available_rom_banks;
+	uint8_t available_ram_banks;
+	_mbc_type mbc_type;
+	uint32_t rom_size;
+	uint32_t ram_size;
+	uint8_t bank_select1;
+	uint8_t bank_select2;
+	uint8_t bank_select3;
+	uint8_t rtc_register;
+	uint8_t* rtc_registers;
+
+	uint16_t curr_rom_bank() const;
+	uint8_t curr_ram_bank() const;
+	void write_to_ex_ram(const uint16_t index, const uint8_t value);
+	void write_to_hram(const uint16_t index, const uint8_t value);
+	void write_to_mbc(const uint16_t index, const uint8_t value);
+	void write_to_mbc1(const uint16_t index, const uint8_t value);
+	void write_to_mbc2(const uint16_t index, const uint8_t value);
+	void write_to_mbc3(const uint16_t index, const uint8_t value);
+	void write_to_mbc5(const uint16_t index, const uint8_t value);
+
 
 public:
 
-_memory(const std::string& rom_name);
-_memory(){}
-~_memory();
+	_memory(const std::string& rom_name);
+	_memory(){}
+	~_memory();
 
-uint8_t& operator[](const uint16_t index);
-const uint8_t& operator[](const uint16_t index) const;
+	bool dma_request;
 
-uint8_t read(const uint16_t index){ return memory[index]; }
-void write(const uint16_t index, const uint8_t value);
+	uint8_t& operator[](const uint16_t index);
+	const uint8_t& operator[](const uint16_t index) const;
+	uint8_t read(const uint16_t index){ return memory[index];}
+	void write(const uint16_t index, const uint8_t value);
+	void save_ram();
+	void load_ram();
 
 };
-
 
 
 #endif
--- src/memory.cpp
+++ src/memory.cpp
@@ -1,26 +1,73 @@
 #include "memory.hh"
 
 _memory::_memory(const std::string& rom_name){
+
 	memory = new uint8_t[65536];
-	rom_banks = new uint8_t[16*65536]; //64 banks
-	curr_bank = 0;
+
+	bank_select1 = 1;
+	bank_select2 = 0;
+	bank_select3 = 0;
+	dummy = 0xFF;
+	dma_time = false;
+	dma_request = false;
+	external_ram = nullptr;
+	rom_banks = nullptr;
+	ram_enable = false;
+	mode_select = 0;
+	rtc_registers = new uint8_t[5];
+	_rom_name = rom_name;
 
 	rom = new std::fstream(rom_name.c_str(), std::ios::in | std::ios::binary);
+
+	if(!rom->is_open())
+		throw std::runtime_error("Error opening game file.");
 
 	rom->seekg(0, rom->end);
 	unsigned int length = rom->tellg();
 	rom->seekg(0, rom->beg);
-	std::cout << std::hex << length << std::endl;
-	if(length <= 0x8000){ //32 KByte
-		rom->read((char*)memory, length);
-		mbc = false;
+
+	rom->read((char*)memory, 0x8000);
+	rom->seekg(0, rom->beg);
+
+	if(memory[0x148] <= 0x6)
+		rom_banks = new uint8_t[length];
+	else
+		throw std::runtime_error("Unsupported rom size.");
+
+	rom->read((char*)rom_banks, length);
+	
+	if(memory[0x147] > 0 && memory[0x147] <=3)
+		mbc_type = mbc1;
+	else if(memory[0x147] == 5 || memory[0x147] == 6)
+		mbc_type = mbc2;
+	else if(memory[0x147] > 0xE && memory[0x147] <= 0x13)
+		mbc_type = mbc3;
+	else if(memory[0x147] >= 0x19 && memory[0x147] < 0x1F)
+		mbc_type = mbc5;
+	else
+		mbc_type = none;
+
+	rom_size = length;
+	ram_type = memory[0x149];
+
+	if(ram_type !=0){
+		if(ram_type > 4)
+			throw std::runtime_error("Unsupported ram size.");
+		uint32_t ram_types[4] = {2048, 8192, 32768, 131072};
+		ram_size = ram_types[ram_type-1];
+		external_ram = new uint8_t[ram_size];
+		ex_ram = true;
 	}
 	else{
-		rom->read((char*)memory, 0x8000);
-		rom->seekg(0, rom->beg);
-		rom->read((char*)rom_banks, length);
-		mbc = true;
-	}
+		ex_ram = false;
+		ram_size = 0;
+		}
+
+
+	available_rom_banks = rom_size / 0x4000;
+	available_ram_banks = ram_type < 3 ? 1 : ram_size / 0x2000;
+
+	load_ram();
 
 }
 
@@ -40,53 +87,376 @@ _memory::~_memory(){
 		rom->close();
 		rom = nullptr;
 	}
+
+	if(external_ram != nullptr){
+		delete[] external_ram;
+		external_ram = nullptr;
+	}
+
+	if(rtc_registers != nullptr){
+		delete[] rtc_registers;
+		rtc_registers = nullptr;
+	}
+
 }
 
+
+
+uint16_t _memory::curr_rom_bank() const{
+
+	if(mbc_type == mbc1)
+		return ((bank_select2 << 5) | bank_select1 ) % available_rom_banks;
+
+	else if(mbc_type == mbc5)
+		return ((bank_select2 << 8) | bank_select1) % available_rom_banks;
+	
+	else
+		return bank_select1 % available_rom_banks;
+	
+}
+
+uint8_t _memory::curr_ram_bank() const{
+
+	if(mbc_type == mbc1){
+
+		if(mode_select == 1)
+			return bank_select2 % available_ram_banks;
+		else
+			return 0;
+	}
+	
+	else if(mbc_type == mbc5)
+		return bank_select3 % available_ram_banks;
+
+	else
+		return bank_select2 % available_ram_banks;
+
+}
+
+
+
+
 uint8_t& _memory::operator[](const uint16_t index){ 
-	if((index >= 0x4000 && index < 0x8000) && mbc) //rom bank 1 area
-		return rom_banks[0x4000 * curr_bank + index - 0x4000];
+
+	if(index <= 0x3FFF){
+
+		if(mbc_type == mbc1){
+			if(mode_select == 0)
+				return memory[index];
+			else
+				return rom_banks[0x4000 * ((bank_select2 << 5) % available_rom_banks) + index];
+		}
+		else
+			return memory[index];			
+	}
+	
+	else if((index >= 0x4000 && index < 0x8000) && mbc_type != none){ //switchable rom bank area
+		return rom_banks[0x4000 * curr_rom_bank() + index - 0x4000];
+	}
+
+	else if((index >= 0xA000 && index < 0xC000)){ //external ram area
+
+		if(ram_enable && ex_ram){
+
+			if(mbc_type == mbc3 && rtc_register > 0)
+				return rtc_registers[rtc_register - 8];
+
+			else if(mbc_type == mbc2)
+				return external_ram[(index - 0xA000) % 0x200];
+
+			else
+				return external_ram[curr_ram_bank() * 0x2000 + index - 0xA000];			
+		}
+		else
+			return dummy;		
+	}
+
 	else
 		return memory[index]; 
 }
 
 const uint8_t& _memory::operator[](const uint16_t index) const {
-	if((index >= 0x4000 && index < 0x8000) && mbc) //rom bank 1 area
-		return rom_banks[0x4000 * curr_bank + index - 0x4000];
-	else
-		return memory[index]; 
+
+	return this->operator[](index);
 }
 
 void _memory::write(const uint16_t index, const uint8_t value){
 
-	if(index >= 0x2000 && index <= 0x3FFF){
-		curr_bank = value & 0x1F;
-		if(value & 0xF == 0)
-			curr_bank |= 0x1;
+	if(dma_time && index < 0xFF80)
+		return;
+
+	if(index < 0x8000)
+		write_to_mbc(index, value);
+
+	else if(index >= 0x8000 && index < 0xA000){ //vram
+		if((memory[0xFF41] & 0x3) < 0x3)
+			memory[index] = value;
 	}
 
-	if(index >= 0x4000 && index <= 0x5FFF){
-		curr_bank |= value << 5;
-	}
+	else if((index >= 0xA000 && index < 0xC000))
+		write_to_ex_ram(index, value);
+	
 
-
-
-	if(index < 0x8000){
-		dummy = value;
-	}
-
-	else if(index >= 0xC000 && index < 0xDE00){
+	else if(index >= 0xC000 && index < 0xDE00){ //echo ram
 		memory[index] = value;
 		memory[0xE000 + index - 0xC000] = value;
 	}
 
-	else if(index >= 0xE000 && index < 0xFE00){
+	else if(index >= 0xE000 && index < 0xFE00){ //echo ram
 		memory[index] = value;
 		memory[0xC000 + index - 0xE000] = value;
 	}
+
+	else if(index >= 0xFE00 && index < 0xFEA0){ //oam ram
+		if((memory[0xFF41] & 0x3) < 0x2)
+			memory[index] = value;
+	}
+
+	else if(index >= 0xFEA0 && index <= 0xFFFF)
+		write_to_hram(index, value);
+
+	else
+		memory[index] = value;
+}
+
+
+void _memory::write_to_ex_ram(const uint16_t index, const uint8_t value){
+
+	if(ram_enable && ex_ram){
+		if(mbc_type == mbc3 && rtc_register > 0)
+			rtc_registers[rtc_register - 8] = value;
+
+		else if(mbc_type == mbc2)
+			external_ram[(index - 0xA000) % 0x200] = value & 0xF;
+
+		else
+			external_ram[curr_ram_bank() * 0x2000 + index - 0xA000] = value;
+		}
+
+}
+
+void _memory::write_to_hram(const uint16_t index, const uint8_t value){
+
+	if(index == 0xFF00)
+		memory[index] = value | 0xCF;
+
+	else if(index == 0xFF03 || index == 0xFF04){
+		memory[0xFF03] = 0;
+		memory[0xFF04] = 0;
+	}
+
+	else if(index == 0xFF40){
+		if(!(value & 1 << 7)){
+			memory[0xFF44] = 0; //LY
+			memory[0xFF41] &= ~0x3; //STAT
+			}
+		memory[index] = value;
+	}
+
+	else if(index == 0xFF41)
+		memory[index] = value | 1 << 7;
+
+	else if(index == 0xFF44)
+		memory[index] = 0;
 	
+	else if(index == 0xFF46){
+		//dma_time = true;
+		dma_request = true;
+		memory[index] = value;
+	}
+
 	else
 		memory[index] = value;
 
 }
+
+void _memory::write_to_mbc(const uint16_t index, const uint8_t value){
+	
+	switch(mbc_type){
+
+		case mbc1:{
+			write_to_mbc1(index, value);
+			break;
+		}
+
+		case mbc2:{
+			write_to_mbc2(index, value);
+			break;
+		}
+
+		case mbc3:{
+			write_to_mbc3(index, value);
+			break;
+		}			
+
+		case mbc5:{
+			write_to_mbc5(index, value);
+			break;
+		}
+
+		default:
+			break;
+	}
+
+}
+
+
+void _memory::write_to_mbc1(const uint16_t index, const uint8_t value){
+
+	if(index <= 0x1FFF){
+		if((value & 0xF) == 0xA)
+			ram_enable = true;
+		else
+			ram_enable = false;
+	}
+
+	else if(index >= 0x2000 && index <= 0x3FFF){
+		bank_select1 = (value & 0x1F);
+		if((value & 0x1F) == 0)
+			bank_select1 = 1;
+	}
+
+	else if(index >= 0x4000 && index <= 0x5FFF){
+		bank_select2 = (value & 0x3);
+	}
+
+	else if(index >= 0x6000 && index <= 0x7FFF){
+		if(value & 0x1)
+			mode_select = 1;
+		else
+			mode_select = 0;
+	}
+
+}
+
+
+void _memory::write_to_mbc2(const uint16_t index, const uint8_t value){
+
+	if(index <= 0x3FFF){
+		if(index & 1 << 8){
+			bank_select1 = (value & 0xF);
+			if(bank_select1 == 0)
+				bank_select1 = 1;
+		}
+		else{
+			if((value & 0xF) == 0xA)
+				ram_enable = true;
+			else
+				ram_enable = false;
+		}
+	}
+
+}
+
+
+void _memory::write_to_mbc3(const uint16_t index, const uint8_t value){
+
+
+	if(index <= 0x1FFF){
+		if((value & 0xF) == 0xA)
+			ram_enable = true;
+		else
+			ram_enable = false;
+	}
+
+	else if(index >= 0x2000 && index <= 0x3FFF){
+		bank_select1 = (value & 0x7F);
+		if((value & 0x7F) == 0)
+			bank_select1 |= 1;
+	}
+
+	else if(index >= 0x4000 && index <= 0x5FFF){
+		if( (value & 0xF) >= 0x8 && (value & 0xF) <= 0xC)
+			rtc_register = value & 0xF;
+		else{
+			bank_select2 = (value & 0x3);
+			rtc_register = 0;
+		}
+	}
+
+	else if(index >= 0x6000 && index <= 0x7FFF){
+		if(value == 0)
+			latch = 1;
+		else if(value == 1){
+			if(latch){
+				//latching function will be there
+				latch = 0;
+			}
+		}
+				
+	}
+
+}
+
+
+void _memory::write_to_mbc5(const uint16_t index, const uint8_t value){
+
+	if(index <= 0x1FFF){
+		if(value == 0xA)
+			ram_enable = true;
+		else
+			ram_enable = false;
+	}
+
+	else if(index >= 0x2000 && index <= 0x2FFF){
+		bank_select1 = value;
+	}
+
+	else if(index >= 0x3000 && index <= 0x3FFF){
+		bank_select2 = (value & 0x1);
+	}
+
+	else if(index >= 0x4000 && index <= 0x5FFF)
+		bank_select3 = (value & 0xF);
+	
+}
+
+
+void _memory::save_ram(){
+
+	if(!ex_ram)
+		return;
+
+	std::string save_name;
+	std::size_t dot = _rom_name.find_last_of('.');
+
+	save_name = _rom_name.substr(0, dot);
+	save_name += ".sav";
+
+	std::fstream save(save_name.c_str(), std::ios::out | std::ios::binary);
+	if(!save.is_open())
+		throw std::runtime_error("Save file creating error.");
+
+	save.write((char*)external_ram, ram_size);
+	save.close();
+
+}
+void _memory::load_ram(){
+
+	if(!ex_ram)
+		return;
+
+	std::string save_name;
+	std::size_t dot = _rom_name.find_last_of('.');
+
+	save_name = _rom_name.substr(0, dot);
+	save_name += ".sav";
+
+	std::fstream save(save_name.c_str(), std::ios::in | std::ios::binary);
+	if(!save.is_open())
+		return;
+
+	save.seekg(0, save.end);
+	unsigned int length = save.tellg();
+	save.seekg(0, save.beg);
+
+	if(length != ram_size)
+		return;
+
+	save.read((char*)external_ram, ram_size);
+	save.close();
+}
+
+
 
 
