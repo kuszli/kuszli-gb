f3f8a03b1e067941b188233ef99cecb47bca1e52
Dawid Olszewski <kuszli@o2.pl>
2019-09-11 16:43:58 +0200

    Added separate interface class

---
+++ inc/sfml_interface.hh
@@ -0,0 +1,42 @@
+#ifndef sfml_interfaceh
+#define sfml_interfaceh
+
+#include <SFML/Graphics.hpp>
+#include <cstdint>
+#include "sfml_buttons.hh"
+
+
+class sfml_interface{
+
+	sf::Vector2u screen_size = sf::Vector2u(480,432);
+	sf::Vector2u oam_screen_size = sf::Vector2u(160,512);
+
+	sf::Color pal[4] = {sf::Color(255,255,255), sf::Color(160,160,160), sf::Color(75,75,75), sf::Color(0,0,0)};
+	sf::RenderWindow window;
+	sf::RenderWindow debug;
+	sf::Texture texture;
+	sf::Texture oam_texture;
+	sf::Sprite sprite;
+	sf::Sprite oam;
+	sf::Event event;
+
+
+	uint8_t* px; 
+	uint8_t* oam_px;
+	uint8_t key;
+	bool oam_dbg;
+
+public:
+
+	sfml_interface(bool dbg);
+	~sfml_interface();
+	uint8_t get_key() const { return key; }
+	void display(uint8_t** pixels);
+	void show_oam(uint8_t** oam_pixels);
+	void check_events();
+	bool is_window_open() const { return window.isOpen(); }
+
+};
+
+
+#endif
---
+++ src/sfml_interface.cpp
@@ -0,0 +1,88 @@
+#include "sfml_interface.hh"
+
+sfml_interface::sfml_interface(bool dbg){
+	window.create(sf::VideoMode(160, 144), "kuszli-gb");
+	window.setSize(screen_size);
+	texture.create(160,144);
+	sprite.setTexture(texture);
+	px = new  uint8_t[160*144*4];
+
+	if(dbg){
+		debug.create(sf::VideoMode(40,128), "oam viewer");
+		debug.setSize(oam_screen_size);
+		oam_texture.create(40,128);
+		oam.setTexture(oam_texture);
+		oam_px = new uint8_t[128*40*4];
+	}
+	
+	key = 0;
+	oam_dbg = dbg;
+}
+
+
+sfml_interface::~sfml_interface(){
+	delete[] px;
+	if(oam_dbg)
+		delete[] oam_px;
+
+}
+
+
+void sfml_interface::display(uint8_t** pixels){
+
+	for(int y = 0; y < 144; ++y){
+		for(int x = 0; x < 160; ++x){
+			px[(y*160 + x)*4] = pal[pixels[y][x]].r;
+			px[(y*160 + x)*4+1] = pal[pixels[y][x]].g;
+			px[(y*160 + x)*4+2] = pal[pixels[y][x]].b;
+			px[(y*160 + x)*4+3] = 255;
+		}	
+	}
+
+	texture.update(px);
+	window.draw(sprite);
+	window.display();
+
+}
+
+
+void sfml_interface::show_oam(uint8_t** oam_pixels){
+
+	if(!oam_dbg)
+		return;
+
+	for(int y = 0; y < 128; ++y){
+		for(int x = 0; x < 40; ++x){
+			oam_px[(y*40 + x)*4] = pal[oam_pixels[y][x]].r;
+			oam_px[(y*40 + x)*4+1] = pal[oam_pixels[y][x]].g;
+			oam_px[(y*40 + x)*4+2] = pal[oam_pixels[y][x]].b;
+			oam_px[(y*40 + x)*4+3] = 255;
+		}
+	}
+
+	oam_texture.update(oam_px);
+	debug.draw(oam);
+	debug.display();
+
+}
+
+void sfml_interface::check_events(){
+
+	while (window.pollEvent(event)){
+		if (event.type == sf::Event::Closed)
+			window.close();
+		else if(event.type == sf::Event::KeyPressed || event.type == sf::Event::KeyReleased)
+			sfml_buttons::read(key);
+	
+	}
+
+	if(oam_dbg){
+		while (debug.pollEvent(event)){
+			if (event.type == sf::Event::Closed)
+				debug.close();
+		}
+	}
+
+}
+
+
